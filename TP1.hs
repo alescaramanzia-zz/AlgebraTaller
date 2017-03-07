4a4dc4bc59f69ad891c9af741940d5088358c737
Alexis Damian Arriondo <alescaramanzia@gmail.com>
2017-03-07 03:05:23 -0300

    TP1 finalizado

--- TP1.hs
+++ TP1.hs
@@ -1,56 +1,103 @@
-{-Trabajo Practico-} -- anda todo bien salvo un detalle en el ejercicio 1 --
+--Trabajo Practico -Parte 1
 
-data Mensaje = TextoClaro Texto | CifradoReverso Mensaje  deriving (Eq , Show)
+--Definiciones propias
+
+data Mensaje = TextoClaro Texto | CifradoReverso Mensaje  
+        deriving (Eq , Show)
+
 type Texto = [Char]
---ejercicio 1 bis-- --Funcion auxiliar esPosibleMensaje--
-esPosibleMensaje :: Texto -> Bool --Cuando Hago esPosibleMensaje "CaracterValido , CaracterNoValido, ...." Toma al 2do caracter como Valido , Si pongo"Caracter No Valido"en cualquier otro lado o mas de 1 Caracter no valido anda bien--
---esPosibelMensaje "CaracterValido,CaracterNoValido" anda bien, solo "CaracterNoValido en la 2da posicion no da error puse posibles formas de matarlo ninguna me anduvo"
 
-esPosibleMensaje []   = False
+--Funciones auxiliares
 
-esPosibleMensaje [x]  | (x `elem` ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']) = True
-                      | otherwise                                                                                                                    = False
+esPosibleMensaje :: Texto -> Bool --Verifica que los caracteres ingresados en el string concuerden con los válidos en el TP usando la función elem para 
+                                  --filtrar las mayúsculas y el espacio (se crea una lista que contenga todos los caracteres posibles para cotejar).
+esPosibleMensaje []                                    = False
+esPosibleMensaje [x]    | x `elem` [' ']++['A'..'Z']   = True
+esPosibleMensaje (x:xs) | x `elem` [' ']++['A'..'Z']   = esPosibleMensaje xs
+                        | otherwise                    = False
+--Ejercicio 1--
 
-esPosibleMensaje [x,y] | (x `elem` ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']) && (y `elem` ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '])  = True
-                       | otherwise                                                                                                                                                                                                                                                     = False
-
-esPosibleMensaje (x:xs) | ((length xs) == 1)                                                                                                                                 = esPosibleMensaje [x]
- --Dos Posible Formas de solucionarlo--                      -- | ((head (tail xs)) `notElem` ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '])     = False --
- --Ninguna anduvo pero ambos corrian bien el programa--      -- | ((esPosibleMensaje [(head xs),(head (tail xs))]) == False)                                                                                         = False --
-                        | (x `elem` ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '])                       = esPosibleMensaje ((head (tail xs)):tail xs) 
-                        | otherwise                                                                                                                                          = False
-
---ejercicio 1--
 crearMensaje :: Texto -> Mensaje
-crearMensaje xs | (esPosibleMensaje xs) == True    = TextoClaro xs
-                | otherwise                        = (TextoClaro ['E','R','R','O','R']) 
---del ejercicio 1 llama solo al error si el 1er elemento es distinto de volcal o espacio, para los demas elementos no anda!, los toma como posibles valores de texto--
+crearMensaje xs | (esPosibleMensaje xs) == True = TextoClaro xs
+                | otherwise                     = (TextoClaro "ERROR") 
+
 --Ejercicio 2--
 
 esMensajeCifrado :: Mensaje -> Bool
-esMensajeCifrado (TextoClaro a) = False
+esMensajeCifrado (TextoClaro a)                  = False
 esMensajeCifrado (CifradoReverso (TextoClaro a)) = True 
 
---ejercicio 3--
+--Ejercicio 3--
 
 cifrarReverso :: Mensaje -> Mensaje
-cifrarReverso (TextoClaro a) = CifradoReverso (TextoClaro (reverse a))
+cifrarReverso (TextoClaro a)                  = CifradoReverso (TextoClaro (reverse a))
 cifrarReverso (CifradoReverso (TextoClaro a)) = CifradoReverso (TextoClaro (reverse a))
 
---ejercicio 4--
+--Ejercicio 4--
+
 extraerMensajeParaEnvio :: Mensaje -> Texto
-extraerMensajeParaEnvio (TextoClaro a) = (reverse a)
+extraerMensajeParaEnvio (TextoClaro a)                  = (reverse a)
 extraerMensajeParaEnvio (CifradoReverso (TextoClaro a)) = a
 
---ejercicio 5--
+--Ejercicio 5--
+
 descifrar :: Mensaje -> Texto
-descifrar (TextoClaro a) = a
+descifrar (TextoClaro a)                  = a
 descifrar (CifradoReverso (TextoClaro a)) = (reverse a)
 
---ejercicio 6-- 
-esAptoReverso :: Mensaje -> Bool
-esAptoReverso (TextoClaro a) | ((a) == (reverse a))                                 = False
-                             | otherwise                                            = True
-esAptoReverso (CifradoReverso (TextoClaro a)) | ((a) == (reverse a))                                 = False
-                                              | otherwise                                            = True
+--Ejercicio 6-- 
 
+esAptoReverso :: Mensaje -> Bool
+esAptoReverso (TextoClaro a)                  | ((a) == (reverse a))                = False
+                                              | otherwise                           = True
+esAptoReverso (CifradoReverso (TextoClaro a)) | ((a) == (reverse a))                = False
+                                              | otherwise                           = True
+
+--Ejemplos de ejecución--
+
+--  11   master  ✔  ghci                              ↑ ✱  ~/g/AlgebraTaller 
+-- GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+-- Loading package ghc-prim ... linking ... done.
+-- Loading package integer-gmp ... linking ... done.
+-- Loading package base ... linking ... done.
+-- Prelude> :l TP1
+-- [1 of 1] Compiling Main             ( TP1.hs, interpreted )
+-- *Main> crearMensaje "PRIMERA PRUEBA"
+-- TextoClaro "PRIMERA PRUEBA"
+-- *Main> crearMensaje " -PRIMERA PRUEBA"
+-- TextoClaro "ERROR"
+-- *Main> crearMensaje "92PRIMERA PRUEBA"
+-- TextoClaro "ERROR"
+-- *Main> esMensajeCifrado (TextoClaro "PRIMERA PRUEBA")
+-- False
+-- *Main> cifrarReverso (TextoClaro "PRIMERA PRUEBA")
+-- CifradoReverso (TextoClaro "ABEURP AREMIRP")
+-- *Main> esMensajeCifrado (CifradoReverso (TextoClaro "ABEURP AREMIRP"))
+-- True
+-- *Main> extraerMensajeParaEnvio (TextoClaro "PRIMERA PRUEBA")
+-- "ABEURP AREMIRP"
+-- *Main> extraerMensajeParaEnvio (CifradoReverso (TextoClaro "ABEURP AREMIRP"))
+-- "ABEURP AREMIRP"
+-- *Main> descifrar (TextoClaro "PRIMERA PRUEBA")
+-- "PRIMERA PRUEBA"
+-- *Main> descifrar (CifradoReverso (TextoClaro "ABEURP AREMIRP"))
+-- "PRIMERA PRUEBA"
+-- *Main> esAptoReverso (TextoClaro "PRIMERA PRUEBA")
+-- True
+-- *Main> esAptoReverso (CifradoReverso (TextoClaro "ABEURP AREMIRP"))
+-- True
+-- *Main> esAptoReverso (TextoClaro "FIBRON")
+-- True
+-- *Main> esAptoReverso ( cifrarReverso ( cifrarReverso ( crearMensaje " OJOTA") ) )
+-- True
+-- *Main> esAptoReverso ( cifrarReverso ( cifrarReverso ( crearMensaje " RECONOCER ") ) )
+-- False
+
+--Bibliografía utilizada
+
+--http://www.haskell.org/hoogle/ (para buscar expresiones y funciones en detalle).
+--Diapositivas del taller
+--http://aprendehaskell.es/
+
+-- Miraglia, Jorge - 666/17 - Jorge10192@live.com.ar
+-- Arriondo, Alexis - 234/17 - alescaramanzia@gmail.com
